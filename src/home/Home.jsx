2afe21addff4da3d95ea9f7f1877b38e5a3df1d2
Marianopiv <marianopividori93@gmail.com>
2023-09-29 01:12:26 -0300

    meto mas cosas

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   };
 
   const handleKeyPress = (event) => {
-    if (event.code === "Space" || event.key === " " || event.keyCode === 32 || event.key === "Enter") {
+    if (event.code === "Space" || event.key === " " || event.keyCode === 32 || event.key === "Enter"||event.key === "Space") {
       if (counter < 1) {
         setDone([...done, { hit, correct: phrase[0].phrase === hit }]);
         setPhrase(filterWordIndex(phrase, counter));
