9aba92a2e192fac45afd02ec0c2b1573e046f7ba
Marianopiv <marianopividori93@gmail.com>
2023-09-29 00:48:00 -0300

    meto mas para celu

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   };
 
   const handleKeyPress = (event) => {
-    if (event.code === "Space"|| event.key === " " || event.keyCode === 32) {
+    if (event.code === "Space" || event.key === " " || event.keyCode === 32 || event.key === "Enter") {
       if (counter < 1) {
         setDone([...done, { hit, correct: phrase[0].phrase === hit }]);
         setPhrase(filterWordIndex(phrase, counter));
@@ -122,7 +122,7 @@ const Home = () => {
           Typing God
         </h1>
         <div className="flex md:gap-4 flex-wrap justify-center font-bold md:text-2xl">
-          <p>Score actual: {score.length > 0 && getFullScore(score)}</p>
+          <p>ScoreMODIFI actual: {score.length > 0 && getFullScore(score)}</p>
           <p>Tiempo restante por frase:{time}</p>
         </div>
         <div className="flex gap-2 flex-wrap relative p-3 items-center">
@@ -132,8 +132,8 @@ const Home = () => {
               <span
                 className={`${
                   word.correct
-                    ? "text-green-500 md:text-4xl rounded-sm"
-                    : "line-through md:text-4xl rounded-sm"
+                    ? "text-green-500 md:text-4xl rounded-sm font-serif"
+                    : "line-through md:text-4xl rounded-sm font-serif"
                 }`}
                 key={index}
               >
